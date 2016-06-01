4d518b5e030e9713330f2c9ff033b36b1bf1b5b1
Jacky Liang <jackyliang@berkeley.edu>
2016-06-01 21:15:01 +0800

    changed urls fr static files on deployment

--- experiments/experiments/urls.py
+++ experiments/experiments/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^trials/', include('trials.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
