bb49f3d9bf211ad69c1ba16a63f6c2d08b998a10
Jacky Liang <jackyliang@berkeley.edu>
2016-06-01 20:12:32 +0800

    requirements picklefield

--- experiments/trials/models.py
+++ experiments/trials/models.py
@@ -1,5 +1,40 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from picklefield.fields import PickledObjectField
+import uuid
 
-# Create your models here.
+class Trial(models.Model):
+    #token
+    token = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    
+    #flags:
+    trial_completed = models.BooleanField(default=False)
+    survey_completed = models.BooleanField(default=False)
+    has_been_verified = models.BooleanField(default=False)
+    
+    #times:
+    creation_time = models.DateTimeField(auto_now_add=True)
+    completion_time = models.DateTimeField(blank=True)
+    
+    #survey:
+    age = models.IntegerField(blank=True)
+    wpm = models.IntegerField(blank=True)
+    video_game_player = models.BooleanField(default=False)
+    experience = models.IntegerField(default=0)
+    email = models.EmailField()
+
+    #data
+    events = PickledObjectField()
+    '''
+    #shared by both types
+    secs
+    is_tedious
+    is_mistake
+    #tedious data
+    intended_key
+    pressed_key
+    is_backspace
+    #relaxed data
+    is_key_down
+    '''  
