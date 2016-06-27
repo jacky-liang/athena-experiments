b4640097df89ac472f47b48b32447ee77f33bcb4
Jacky Liang <jackyliang@berkeley.edu>
2016-06-26 18:00:55 -0700

    migration for id and uid

--- experiments/trials/models.py
+++ experiments/trials/models.py
@@ -6,7 +6,8 @@ import uuid
 
 class Trial(models.Model):
     #id
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    id = models.AutoField(primary_key=True)
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     
     #verification token
     token = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False, unique=True)
