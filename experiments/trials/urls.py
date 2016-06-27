921369184961636fe2cc92170f725f4e8f6998f4
Jacky Liang <jackyliang@berkeley.edu>
2016-06-26 22:52:22 -0700

    finished routes and logics for start, token, and verify

--- experiments/trials/urls.py
+++ experiments/trials/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^start/$', views.start, name='start'),
     url(r'^survey/$', views.survey, name='survey'),
+    url(r'^run/$', views.run, name='run'),
+    url(r'^verify/$', views.verify, name='verify'),
+    url(r'^token/$', views.token, name='token'),
     url('r^has_been_completed/$', views.has_been_completed, name='has_been_completed')
 ]
--- experiments/trials/views.py
+++ experiments/trials/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import Http404
+from django.http import Http404, JsonResponse, HttpResponseForbidden
 from trials.models import Trial
+import json
+
+#TODO: ONLY FOR DEBUG
+from django.views.decorators.csrf import csrf_exempt
 
 def _get_id(request):
     id = request.GET.get('id', '')
@@ -30,7 +34,7 @@ def has_been_completed(request, portion='', id=''):
     
 def start(request):
     trial = Trial.objects.create()
-    return redirect('trials/survey/?id={0}'.format(trial.id))
+    return redirect('/trials/survey/?id={0}'.format(trial.id))
     
 def survey(request):
     id = _get_id(request)
@@ -38,11 +42,62 @@ def survey(request):
         
     if trial.survey_completed:
         return redirect('has_been_completed', portion='survey', id=id)
-    if trial.survey_completed:
+    if trial.trial_completed:
         return redirect('has_been_completed', portion='entirety', id=id)
         
     context = {
-        'title': 'Experiments Home',
+        'title': 'Experiments Survey',
         'id': id
     }
     return render(request, 'trials/survey.html', context)
+    
+def run(request):
+    id = _get_id(request)
+    trial = _get_trial(id)
+    
+    if trial.trial_completed:
+        return redirect('has_been_completed', portion='entirety', id=id)
+    
+    context = {
+        'title': 'Experiments Run',
+        'id': id
+    }
+    
+    return render(request, 'trials/run.html', context)
+    
+def token(request):
+    id = _get_id(request)
+    trial = _get_trial(id)
+    
+    if trial.trial_completed:
+        return JsonResponse({'token':trial.token})
+    else:
+        return HttpResponseForbidden()
+        
+@csrf_exempt
+def verify(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        token = data['token']
+        
+        try:
+            trial = Trial.objects.get(token=token)
+        except Trial.DoesNotExist:
+            return JsonResponse({
+                'valid': False
+            })
+
+        completed = trial.trial_completed
+        has_been_verified = trial.has_been_verified
+        
+        if not has_been_verified:
+            trial.has_been_verified = True
+            trial.save()
+            
+        return JsonResponse({
+            'valid': True,
+            'completed': completed,
+            'has_been_verified': has_been_verified
+        })
+    else:
+        raise Http404('Page not found')
