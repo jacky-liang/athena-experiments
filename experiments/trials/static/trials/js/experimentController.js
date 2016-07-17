668c4f20b7c20722aaecdd982b6d59f435d034c5
Daniel Yuan <daniel1yuan@gmail.com>
2016-07-17 16:57:48 -0500

    Finished changing experiment controller for new trial structure

--- experiments/trials/static/trials/js/experimentController.js
+++ experiments/trials/static/trials/js/experimentController.js
@@ -1,11 +1,12 @@
 //Global Variables
 var gameData = {lastSwitch: 0};
 var gameMode = {tedious: 0, relaxed: 1};
-var experimentData = {experimentType: 0, taskDuration: 0.5};	//ExperimentType not used
-var experimentDuration = 5; //Time in minutes
-var stringLength = 7;
-var numString = 5;
+var experimentData = {events: [], taskDuration: 0.5, experimentType: 0};
+var maxStringLength = 10;
+var minStringLength = 5;
+var numString = 1;
 var numCircles = 4;
+var experimentDuraction = null;
 
 //Initializes the game
 function initGame(){
@@ -27,6 +28,7 @@ function initGame(){
 function tediousTask(){
 	$('#gameWindow').empty();
 	$('#userInput').css("display", "block");
+	$('#scoreWindow').css("display", "block");
 	gameData.taskCount = numString;
 	gameData.tediousArray = [];
 
@@ -34,7 +36,7 @@ function tediousTask(){
 	for(var i = 0; i < numString; i++){
 		var curString = generateString();
 		gameData.tediousArray.push(curString);
-		var divsize = ((Math.random()*100) + 50).toFixed();
+		var divsize = 15 * maxStringLength;
 		var posx = (Math.random() * ($('#gameWindow').width() - divsize)).toFixed();
     var posy = (Math.random() * ($('#gameWindow').height() - divsize)).toFixed();
 		$newdiv = $("<div id =" + curString + " class = \"tediousTask\">" + curString + "</div>").css({
@@ -42,6 +44,10 @@ function tediousTask(){
       'left':posx+'px',
       'top':posy+'px',
     });
+		gameData.curChar = curString[0];
+		gameData.curString = curString;
+		gameData.playerString = "";
+		gameData.prevString = "";
 		$('#gameWindow').append($newdiv);
 	}
 }
@@ -49,6 +55,7 @@ function tediousTask(){
 function relaxedTask(){
 	$('#gameWindow').empty();
 	$('#userInput').css("display", "none");
+	$('#scoreWindow').css("display", "none");
 
 	gameData.taskCount = numCircles;
 	var circleTypes = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
@@ -63,31 +70,55 @@ function relaxedTask(){
 //Handles the tedious Task inputs
 function getUserText(){
 	var input = $('#userInput').val();
-
-	//If the user hasn't pressed enter yet, do nothing
-	if (input.indexOf("\n") == -1){
+	if (input == ""){
 		return;
-	}else{	//User pressed enter, check for any matches with the generated words
-		var userInput = (input.split("\n"))[0];
-		for (var i in gameData.tediousArray){
-			if(gameData.tediousArray[i] == userInput){	//If user typed in a right word, determine which game to switch to
-				gameData.score++;
-				updateScore();
-				$('#' + userInput).remove();
-				gameData.taskCount--;
-				storeExperimentData();
-				if (isTimeUp()){
-					endExperiment();
-					return;
-				}
-				if (gameData.taskCount == 0){
-    			startTask(chooseTask());
-    		}
-			}
-		}
-		//Clear text area if user didn't match anything
-		$('#userInput').val("");
 	}
+	var curString = gameData.curString;
+	if (input.length > 1){
+		if (gameData.prevString.length - input.length == 1){
+			storeTediousData(gameData.curChar, null, true, false);
+		}else{
+			storeTediousData(gameData.curChar, input[input.length-1], false, false);
+		}
+		gameData.prevString = input; 
+		return;
+	}else if (input == gameData.curChar){
+		//If the user has typed same character as the current character -> color the character green and move on
+		storeTediousData(gameData.curChar, input[input.length-1], false, true);
+		gameData.playerString = gameData.playerString + input;
+		//End of the tedious task, finished completing the string
+		if (gameData.playerString == gameData.curString){
+			gameData.score++;
+			updateScore();
+			$('#' + curString).remove();
+			$('#userInput').val("");
+			gameData.taskCount--;
+			if (isTimeUp()){
+				endExperiment();
+				return;
+			}
+			if (gameData.taskCount == 0){
+				startTask(chooseTask());
+			}
+		}else{
+			//The string has not been completed yet -> mark completed part green and leave the rest untouched
+			var endString = curString.substring(gameData.playerString.length);
+			$('#' + curString).html('<span style="color: green">'+ gameData.playerString + '</span>' + endString);
+			$('#userInput').val("");
+			gameData.curChar = curString.substring(gameData.playerString.length, gameData.playerString.length + 1);
+		}	
+	}else{
+		//This current character is incorrect -> color it red
+		if (gameData.prevString.length - input.length == 1){
+			storeTediousData(gameData.curChar, null, true, false);
+		}else{
+			storeTediousData(gameData.curChar, input[input.length-1], false, false);
+		}
+		var endString = curString.substring(gameData.playerString.length + 1);
+		var incorrectChar = curString.substring(gameData.playerString.length, gameData.playerString.length + 1);
+		$('#' + curString).html('<span style="color: green">'+ gameData.playerString + '</span>' + '<span style="color: red">'+ incorrectChar + '</span>' + endString);	
+	}
+	gameData.prevString = input; 
 }
 
 function makeCircle(num, divsize, type){
@@ -111,9 +142,16 @@ function makeCircle(num, divsize, type){
     if (type == ""){
     	otherType = "sibling";
     }
-    $newdiv.draggable();
+    $newdiv.draggable({
+			start: function(){
+				storeRelaxedData(true, false, false);
+			},
+			stop: function(){
+				storeRelaxedData(false, true, false);
+			},
+		});
     $newdiv.droppable({
-    	tolerance: "touch",
+    	tolerance: "intersect",
     	accept: '#circle' + num + otherType,
     	hoverClass: "hover",
     	drop: function(event, ui){
@@ -123,7 +161,7 @@ function makeCircle(num, divsize, type){
 					endExperiment();
 					return;
 				}
-    		storeExperimentData();
+				storeRelaxedData(false, true, true);
     		gameData.taskCount--;
     		if (gameData.taskCount == 0){
     			startTask(chooseTask());
@@ -133,6 +171,12 @@ function makeCircle(num, divsize, type){
     
     $newdiv.prepend('<img src="' + ENV.ASSET_PATH + 'circle' + num + '.png" class = "circleImg"/>');
     $('#gameWindow').append($newdiv);
+}
+
+//Returns time in seconds
+function getExperimentTime(){
+	var curTime = new Date();
+	return ((curTime.getTime() - experimentData.startTime.getTime())/1000);
 }
 
 //Returns time in minutes
@@ -184,7 +228,13 @@ function generateString()
 {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		var stringLength = Math.round(Math.random() * maxStringLength);
 
+		//Keep generating stringLengths until it matches the size we want
+		while(stringLength < minStringLength){
+			stringLength = Math.round(Math.random() * maxStringLength);
+		}
+	
     for( var i=0; i < stringLength; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
@@ -210,6 +260,22 @@ function shuffle(array) {
 function endExperiment(){
 	$('#gameWindow').empty();
 	$('#gameWindow').append("<div>Experiment is over..</div>");
+	submit_event_url = "/trials/data/events/?id=" + ENV.TRIAL_ID;
+	$.ajax({
+		type: "POST",
+		url: submit_event_url,
+		contentType: 'application/json',
+		data: JSON.stringify(experimentData.events),
+		dataType: 'json',
+		success: function(res){
+			if (res.success){
+				complete_url = '/trials/complete/?id=' + ENV.TRIAL_ID;
+				window.location=complete_url;
+			}else{
+				alert("YOU MESSED UP BUD");
+			}
+		}
+	});
 }
 
 //Updates the score Window
@@ -218,11 +284,38 @@ function updateScore(){
 	$('#scoreWindow').append("Score: " + gameData.score);
 }
 
-//Stores experiment data
-function storeExperimentData(){
+function storeRelaxedData(drag, release, correct){
+	var Event = {
+		'Timestamp': getExperimentTime(),
+		'Action': {
+			'Tedious': null,	
+			'Relaxed': {
+				'circle_drag': drag,
+				'circle_release': release,
+				'result': correct
+			}
+		}
+	};
+	experimentData.events.push(Event);
+}
 
+function storeTediousData(intendedChar, pressedChar, isBackspace, correct){
+	var Event = {
+		'Timestamp': getExperimentTime(),
+		'Action': {
+			'Relaxed': null,	
+			'Tedious': {
+				'intended_char': intendedChar,
+				'pressed_char': pressedChar,
+				'is_backspace': isBackspace,
+				'result': correct
+			}
+		}
+	};
+	experimentData.events.push(Event);
 }
 
 $(document).ready(function(){
+	experimentDuration = ENV.TRIAL_LENGTH; //Time in minutes
 	initGame();
 });
